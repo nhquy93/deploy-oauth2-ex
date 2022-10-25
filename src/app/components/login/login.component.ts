98f44563f589b478242a94cc03dc33864323f77c
anh.nguyentuan <anh.nguyentuan@petronas.com>
2022-10-25 21:07:30 +0700

    four commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { FacebookLoginProvider, GoogleLoginProvider, MicrosoftLoginProvider, SocialAuthService } from 'angularx-social-login';
-import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { Component, OnInit } from "@angular/core";
+import {
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  MicrosoftLoginProvider,
+  SocialAuthService,
+} from "angularx-social-login";
+import { MdbModalRef } from "mdb-angular-ui-kit/modal";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   title: String | null;
   GoogleLoginProvider = GoogleLoginProvider;
-  constructor(public modalRef: MdbModalRef<LoginComponent>,private socialAuthService:SocialAuthService ) {}
+  constructor(
+    public modalRef: MdbModalRef<LoginComponent>,
+    private socialAuthService: SocialAuthService
+  ) {}
 
   ngOnInit(): void {}
 
   signInWithGoogle() {
     this.modalRef.close();
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    this.socialAuthService
+      .signIn(GoogleLoginProvider.PROVIDER_ID)
+      .catch((e) => {
+        return;
+      });
   }
   loginWithFacebook(): void {
     this.modalRef.close();
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    this.socialAuthService
+      .signIn(FacebookLoginProvider.PROVIDER_ID)
+      .catch((e) => {
+        return;
+      });
   }
   loginWithMicrosoft(): void {
     this.modalRef.close();
-    this.socialAuthService.signIn(MicrosoftLoginProvider.PROVIDER_ID);
+    this.socialAuthService
+      .signIn(MicrosoftLoginProvider.PROVIDER_ID)
+      .catch((e) => {
+        return;
+      });
   }
 }
